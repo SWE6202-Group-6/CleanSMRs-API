97bc2d9f69bddbba31f6e39371b9a739d33a240b
Deborah Johnson <deborahjohnsonere@gmail.com>
2024-11-21 15:12:20 +0000

    created observation schema

--- schemas.py
+++ schemas.py
@@ -5,3 +5,30 @@ from flask_marshmallow import Marshmallow
 import app
 
 ma = Marshmallow(app)
+
+class ObservationSchema(ma.SQLAlchemyAutoSchema):     
+    """Definition used by serialization library based on User Model"""
+    class Meta:         
+        fields = ("id",
+                  "date_logged",
+                  "time_logged", 
+                  "time_zone_offset", 
+                  "latitude", 
+                  "longitude", 
+                  "water_temp", 
+                  "air_temp", 
+                  "wind_speed", 
+                  "wind_direction", 
+                  "humidity", 
+                  "haze_percent", 
+                  "precipitation_mm", 
+                  "radiation_bq")  
+        
+        # instantiate objs based on Marshmallow schemas 
+obervation_schema     = ObservationSchema() 
+obervations_schema    = ObservationSchema(many=True)
+
+
+
+
+
