d23cd0aad3142bc821da036a6e38dac006111e49
Joel Heaton <joel.j.heaton@gmail.com>
2024-11-21 10:03:19 +0000

    Move example route into routes file and register the blueprint

--- app.py
+++ app.py
@@ -15,20 +15,11 @@ app = Flask(__name__)
 # Configure the app from the loaded configuration
 app.config["SQLALCHEMY_DATABASE_URI"] = config["SQLALCHEMY_DATABASE_URI"]
 app.config["SQLALCHEMY_ECHO"] = config["SQLALCHEMY_ECHO"]
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = config[
-    "SQLALCHEMY_TRACK_MODIFICATIONS"
-]
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = config["SQLALCHEMY_TRACK_MODIFICATIONS"]
 
 # Initialise the database and Marshmallow
 db.init_app(app)
 ma.init_app(app)
 
-
-@app.get("/")
-def hello_world():
-    """Placeholder endpoint for testing.
-
-    Returns:
-        string: Hello World as a string.
-    """
-    return "Hello World"
+# Register the API blueprint for our route handlers
+app.register_blueprint(api_blueprint, url_prefix="/api")
--- routes.py
+++ routes.py
@@ -4,3 +4,13 @@ from flask import Blueprint
 
 # Create a Flask Blueprint for the routes
 api = Blueprint("api", __name__)
+
+
+@api.route("/hello")
+def hello_world():
+    """Placeholder endpoint for testing.
+
+    Returns:
+        string: Hello World as a string.
+    """
+    return "Hello World"
