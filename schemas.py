8fa332cb86a061a7dd1f92a9d53288162558570d
Joel Heaton <joel.j.heaton@gmail.com>
2024-11-22 19:41:04 +0000

    Quick formatting fix

--- schemas.py
+++ schemas.py
@@ -6,29 +6,30 @@ import app
 
 ma = Marshmallow(app)
 
-class ObservationSchema(ma.SQLAlchemyAutoSchema):     
+
+class ObservationSchema(ma.SQLAlchemyAutoSchema):
     """Definition used by serialization library based on User Model"""
-    class Meta:         
-        fields = ("id",
-                  "date_logged",
-                  "time_logged", 
-                  "time_zone_offset", 
-                  "latitude", 
-                  "longitude", 
-                  "water_temp", 
-                  "air_temp", 
-                  "wind_speed", 
-                  "wind_direction", 
-                  "humidity", 
-                  "haze_percent", 
-                  "precipitation_mm", 
-                  "radiation_bq")  
-        
-        # instantiate objs based on Marshmallow schemas 
-obervation_schema     = ObservationSchema() 
-obervations_schema    = ObservationSchema(many=True)
+
+    class Meta:
+        """Metadata for the ObservationSchema."""
+
+        fields = (
+            "id",
+            "date_logged",
+            "time_logged",
+            "time_zone_offset",
+            "latitude",
+            "longitude",
+            "water_temp",
+            "air_temp",
+            "wind_speed",
+            "wind_direction",
+            "humidity",
+            "haze_percent",
+            "precipitation_mm",
+            "radiation_bq",
+        )
 
 
-
-
-
+obervation_schema = ObservationSchema()
+obervations_schema = ObservationSchema(many=True)
